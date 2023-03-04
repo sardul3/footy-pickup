05b698bd6cc2f10590a36cef7807864e5e027781
sagar <poudelsagar530@gmail.com>
2023-03-04 13:52:44 -0600

    Initial commit for the backend project

---
+++ src/main/java/com/sardul3/footypickup/FootyPickupApplication.java
@@ -0,0 +1,13 @@
+package com.sardul3.footypickup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootyPickupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootyPickupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sardul3/footypickup/FootyPickupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sardul3.footypickup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootyPickupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
